7fcf60319c83683d6f94bbcfec28eaac67bf7a09
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-03-12 00:13:55 +0530

    [Jyoti Rao] Add multiple employees in payroll database without thread

---
+++ EmployeePayrollMultiThreading/EmployeePayRollOperations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollMultiThreading
+{
+    public class EmployeePayRollOperations
+    {
+        public  List<EmployeeDetails> employeePayrollDetailsList = new List<EmployeeDetails>();
+        /// <summary>
+        /// UC1- Without Use of Thread
+        /// </summary>
+        public void AddEmployeeToPayroll(List<EmployeeDetails> employeePayrollDataList)
+        {
+            employeePayrollDataList.ForEach(employeeData =>
+            {
+                Console.WriteLine("Employee being added: " + employeeData.Name);
+                this.AddEmployeePayroll(employeeData);
+                Console.WriteLine("Employee added: "+ employeeData.Name);
+            });
+            Console.WriteLine(this.employeePayrollDetailsList.Count());
+        }
+        public void AddEmployeePayroll(EmployeeDetails emp)
+        {
+            employeePayrollDetailsList.Add(emp);
+        }
+    }
+}
---
+++ TestEmployeePayrollMultiThreading/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestEmployeePayrollMultiThreading")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestEmployeePayrollMultiThreading")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("473ef397-3e0e-4386-b4c5-ac97c1a5688a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
